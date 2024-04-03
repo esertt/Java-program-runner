4ca5016852f179f28896e97e1c013fe1d4732c7b
Eser Tekin Tekeli <tekelieser@gmail.com>
2024-04-03 17:27:21 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.net.http.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) throws InterruptedException, URISyntaxException, IOException {
+        String ApiKey = "4f8d47ee48mshab7519ff03151cfp1e0088jsn65e7c24705a0";
+        String ApiHost = "online-code-compiler.p.rapidapi.com";
+        String ContentType = "application/json";
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your code:");
+        String code = in.nextLine();
+        in.close();
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(new URI("https://online-code-compiler.p.rapidapi.com/v1/"))
+        .header("content-type", ContentType)
+        .header("X-RapidAPI-Key", ApiKey)
+        .header("X-RapidAPI-Host", ApiHost)
+        .method("POST", HttpRequest.BodyPublishers.ofString("{\r"+
+        "\"language\": \"python3\",\r"+
+        "\"version\": \"latest\",\r"+
+        "\"code\": " + "\"" + code+ "\",\r"+
+        "\"input\": null\r}"))
+        .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(extractOutput(response));
+    }
+    
+    public static String extractOutput(HttpResponse<String> res){
+        String output = "";
+        int indexStart = res.body().indexOf("\"output\"");
+        int indexEnd = res.body().indexOf(",\"language\"");
+        output = res.body().substring(indexStart + 10, indexEnd - 1);
+
+        return output;
+    }
+}
